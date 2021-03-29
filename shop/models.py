751580db606728fb82fe6b28ab3b205bf95a5870
prasad <prasadiyer12345@gmail.com>
2021-03-29 20:12:20 +0530

    desc

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200, default="")
+    description = models.CharField(max_length=80, default="")
+    detailed_description = models.TextField(max_length=2000, default="")
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
